bdef93f0047b9bf5228989f5af51ec8abf419040
Kostia1507 <90578275+Kostia1507@users.noreply.github.com>
2024-08-13 13:00:28 +0300

    import currency cog

---
+++ cogs/CurrencyCog.py
@@ -0,0 +1,37 @@
+from discord.ext import commands, tasks
+
+from cogs import LogCog
+from service.localeService import getLocale
+
+from service import currencyService
+
+
+class CurrencyCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.getCurrency.start()
+        LogCog.logSystem("CurrencyCog started")
+
+    @commands.command()
+    async def currency(self, ctx, value: float, fromCurrency: str, *args: str):
+        try:
+            fromCurrency = fromCurrency.upper()
+            res = value / currencyService.currency[fromCurrency]
+        except KeyError:
+            await ctx.send(getLocale("wrong", ctx.author.id) + " " + str(fromCurrency))
+            return 0
+        try:
+            output = f'{value}{fromCurrency}'
+            for loop in args:
+                toCurrency = loop.upper()
+                currentRes = res * currencyService.currency[toCurrency]
+                output = f'{output}={round(currentRes, 4)}{toCurrency}'
+            await ctx.send(output)
+        except KeyError:
+            await ctx.send(getLocale("wrong", ctx.author.id) + " " + str(toCurrency))
+            return 0
+
+    @tasks.loop(hours=12)
+    async def getCurrency(self):
+        await currencyService.updateCurrency()
