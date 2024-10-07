8f791cbd83df5e2edf087d266367aa9ed61fa1ef
Kostia1507 <90578275+Kostia1507@users.noreply.github.com>
2024-10-07 10:45:54 +0300

    develop animate history method

--- models/FourInRowGame.py
+++ models/FourInRowGame.py
@@ -1,6 +1,13 @@
 import random
 import copy
 from datetime import datetime
+from io import BytesIO
+from typing import Any, Union
+
+from PIL import ImageDraw, Image
+from PIL.Image import Image as IMG
+
+from service.TransparertAnimatedGifConverter import TransparentAnimatedGifConverter
 
 numbers = [":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:"]
 POINTS_FOR_CENTER = 3
@@ -275,3 +282,58 @@ class FourInRowGame:
                 if beta <= alpha:
                     break  # Відсікання
             return min_eval, min_line
+
+    async def animate_history(self):
+        SIZE = 50
+        GRAY_COLOR = "#ef1346"
+        BLUE_COLOR = "#16b1ea"
+        RED_COLOR = "#ef1346"
+
+        width, height = self.width * SIZE, self.height * SIZE
+
+        frames = [Image.new('RGBA', size=(width, height), color=(0, 0, 0, 0))]
+        current_height = []
+        for i in range(0, self.width):
+            current_height.append(0)
+
+        for move in range(0, len(self.history)):
+            canvas = frames[-1].copy()
+            draw = ImageDraw(canvas)
+            current_color = BLUE_COLOR if move % 2 == 0 else RED_COLOR
+            draw.ellipse(((self.history[move]*50, height-current_height[self.history[move]]*50),
+                          ((self.history[move]+1)*50, height-(current_height[self.history[move]]+1)*50)),
+                         fill=current_color)
+            frames.append(canvas)
+
+        gif_image, save_kwargs = await self.__animate_gif(frames)
+
+        buffer = BytesIO()
+        gif_image.save(buffer, **save_kwargs)
+        buffer.seek(0)
+
+        return buffer
+
+    async def __animate_gif(self, images: list[IMG], durations: Union[int, list[int]] = 20) -> tuple[
+        IMG, dict[str, Any]]:
+        save_kwargs: dict[str, Any] = {}
+        new_images: list[IMG] = []
+
+        for frame in images:
+            thumbnail = frame.copy()
+            thumbnail_rgba = thumbnail.convert(mode='RGBA')
+            thumbnail_rgba.thumbnail(size=frame.size, reducing_gap=3.0)
+            converter = TransparentAnimatedGifConverter(img_rgba=thumbnail_rgba)
+            thumbnail_p = converter.process()
+            new_images.append(thumbnail_p)
+
+        output_image = new_images[0]
+        save_kwargs.update(
+            format='GIF',
+            save_all=True,
+            optimize=False,
+            append_images=new_images[1:],
+            duration=durations,
+            disposal=2,  # Other disposals don't work
+            loop=0)
+
+        return output_image, save_kwargs
